843218b07172eaf8c65dcae86dbf2a14da030987
farrar142 <gksdjf1690@gmail.com>
2024-12-10 15:22:36 +0900

    feature: 리절트의 모노이드 테스트 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from unittest import main
 
 if __name__ == "__main__":
     from maybe.test import TestMaybe, TestMaybeMonoid
-    from result.test import TestResult
+    from result.test import TestResult, TestResultMonoid
     from context.test import TestContext
     from state.test import TestState
     from delay.test import TestDelay
