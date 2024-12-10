9935bfe1b144149f59382c2520e33d8435912a39
farrar142 <gksdjf1690@gmail.com>
2024-12-10 11:15:38 +0900

    fix: 펑터에 주석 추가

--- functor/__init__.py
+++ functor/__init__.py
@@ -2,6 +2,18 @@ from typing import Self
 
 
 class Functor[V]:
+    """
+    Functor란 범주 A와 B 에 있어 A를 B로 변화시키는 것이다.
+    ex) 1 * Functor = "1"
+    ex) [1,2,3] * Functor => {1:1,2:2,3:3}
+    ex) [1,2,3] => Functor<[1,4,9]>
+    Endofunctor 범주 A를 다시 범주 A로 변환시킨다
+    ex) 1 * Endofunctor => 1
+    ex) [1,2,3] * Endofunctor => [1,2,3]
+    ex) [1,2,3] * Endofunctor => [1,4,9]
+    ex) Maybe * Endofunctor => Maybe
+    """
+
     value: V
 
     def __init__(self, value: V):
